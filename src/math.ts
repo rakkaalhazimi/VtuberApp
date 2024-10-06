12568e12a358f42bd8218785c6b817322aa4ae8f
Rakka Alhazimi <rakkakeren@gmail.com>
2024-10-06 14:34:11 +0700

    added: gradient2D

--- src/math.ts
+++ src/math.ts
@@ -5,3 +5,10 @@ export function euclideanDistance2D(a: number[], b: number[]) {
     Math.pow(a[1] - b[1], 2)
   );
 }
+
+export function gradient2D(a: number[], b: number[]) {
+  let deltaX = b[0] - a[0];
+  let deltaY = b[1] - a[1];
+  
+  return deltaY / deltaX;
+}
