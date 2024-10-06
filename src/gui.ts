dd3e7c6c7f70fb3785441acb5039987de2f2b4b2
Rakka Alhazimi <rakkakeren@gmail.com>
2024-10-06 14:35:35 +0700

    added: bone gui

--- src/gui.ts
+++ src/gui.ts
@@ -19,6 +19,43 @@ export class AppGUI {
         model.morph(key, value);
       })
     }
+    morphGUI.close();
+    console.log(controls);
+  }
+  
+  createBoneGUI(model: Model) {
+    let boneGUI = this.gui.addFolder('bone');
+    let controls = {}
+    for (let key in model.boneDict) {
+      
+      if (key.includes('肩.L')) {
+        continue;
+      }
+      
+      let boneItemGUI = boneGUI.addFolder(key);
+      
+      let rotationXControl = `${key}-rotation-x`
+      controls[rotationXControl] = model.boneDict[key].rotation.x;
+      //@ts-expect-error
+      boneItemGUI.add(controls, rotationXControl, -2.0, 2.0, 0.01).onChange((value: number) => {
+        model.boneDict[key].rotation.x = value;
+      });
+      
+      let rotationYControl = `${key}-rotation-y`
+      controls[rotationYControl] = model.boneDict[key].rotation.y;
+      //@ts-expect-error
+      boneItemGUI.add(controls, rotationYControl, -2.0, 2.0, 0.01).onChange((value: number) => {
+        model.boneDict[key].rotation.y = value;
+      });
+      
+      let rotationZControl = `${key}-rotation-z`
+      controls[rotationZControl] = model.boneDict[key].rotation.z;
+      //@ts-expect-error
+      boneItemGUI.add(controls, rotationZControl, -2.0, 2.0, 0.01).onChange((value: number) => {
+        model.boneDict[key].rotation.z = value;
+      });
+      
+    }
     console.log(controls);
   }
   
