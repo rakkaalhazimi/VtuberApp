89dc92a34e2ddc24087188dd77d80dc1b69befce
Rakka Alhazimi <rakkakeren@gmail.com>
2024-10-13 18:30:58 +0700

    added: blazepose keypoints

--- src/pose-estimation.ts
+++ src/pose-estimation.ts
@@ -19,6 +19,49 @@ const blazePoseConnections = [
   [16, 18], [16, 20], [16, 22]
 ];
 
+export enum BlazePosePoint {
+  NOSE,
+  LEFT_EYE_INNER,
+  LEFT_EYE,
+  LEFT_EYE_OUTER,
+  RIGHT_EYE_INNER,
+  RIGHT_EYE,
+  RIGHT_EYE_OUTER,
+  LEFT_EAR,
+  RIGHT_EAR,
+  MOUTH_LEFT,
+  MOUTH_RIGHT,
+  LEFT_SHOULDER,
+  RIGHT_SHOULDER,
+  LEFT_ELBOW,
+  RIGHT_ELBOW,
+  LEFT_WRIST,
+  RIGHT_WRIST,
+  LEFT_PINKY,
+  RIGHT_PINKY,
+  LEFT_INDEX,
+  RIGHT_INDEX,
+  LEFT_THUMB,
+  RIGHT_THUMB,
+  LEFT_HIP,
+  RIGHT_HIP,
+  LEFT_KNEE,
+  RIGHT_KNEE,
+  LEFT_ANKLE,
+  RIGHT_ANKLE,
+  LEFT_HEEL,
+  RIGHT_HEEL,
+  LEFT_FOOT_INDEX,
+  RIGHT_FOOT_INDEX,
+  // BODYCENTER,
+  // FOREHEAD,
+  // LEFTTHUMB,
+  // LEFTHAND,
+  // RIGHTTHUMB,
+  // RIGHTHAND,
+};
+
+
 // MoveNet keypoint pairs to connect for drawing the skeleton
 const movenetPoseConnections = [
   // Head and Face
